059000507e2da4e9c1c4be3d9ac6868a6d7b1ba1
Muhammad Aiman Yusuf <aimnyusf01@gmail.com>
2025-07-06 23:51:56 +0800

    fixed collapse menu button

--- resources/views/layouts/sections/navbar/navbar.blade.php
+++ resources/views/layouts/sections/navbar/navbar.blade.php
@@ -16,23 +16,12 @@ $navbarDetached = ($navbarDetached ?? '');
     <div class="{{$containerNav}}">
       @endif
 
-      <!--  Brand demo (display only for navbar-full and hide on below xl) -->
-      @if(isset($navbarFull))
-      <div class="navbar-brand app-brand demo d-none d-xl-flex py-0 me-4">
-        <a href="{{url('/')}}" class="app-brand-link gap-2">
-          <span
-            class="app-brand-logo demo">@include('_partials.macros', ["width" => 25, "withbg" => 'var(--bs-primary)'])</span>
-          <span class="app-brand-text demo menu-text fw-bold text-heading">{{config('variables.templateName')}}</span>
-        </a>
-      </div>
-      @endif
-
       <!-- ! Not required for layout-without-menu -->
       @if(!isset($navbarHideToggle))
       <div
         class="layout-menu-toggle navbar-nav align-items-xl-center me-4 me-xl-0{{ isset($menuHorizontal) ? ' d-xl-none ' : '' }} {{ isset($contentNavbar) ? ' d-xl-none ' : '' }}">
         <a class="nav-item nav-link px-0 me-xl-6" href="javascript:void(0)">
-          <i class="bx bx-menu bx-md"></i>
+          <i class="ti ti-menu-2 bx-md"></i>
         </a>
       </div>
       @endif
