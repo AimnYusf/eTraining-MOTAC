ee85251cdbe030ab8d5749585bb6cf07e0ae5820
yusuf <aimnyusf01@gmail.com>
2025-07-17 15:44:03 +0800

    updated 'Rekod Bahagian' interface

--- resources/views/pages/laporan-bahagian.blade.php
+++ resources/views/pages/laporan-bahagian.blade.php
@@ -17,19 +17,15 @@
 @endphp
 
 @section('content')
-  <!-- Record List -->
-  <div class="card mb-8">
 
-    <div class="card-header d-flex flex-wrap justify-content-between gap-4">
-    <div class="card-title mb-0 me-1 d-flex justify-content-between align-items-center">
-      <h5 class="mb-0 flex-grow-1">Rekod Keseluruhan</h5>
-    </div>
-    </div>
-    <div class="card-body">
-    <form method="GET" class="mb-4">
-      <div class="d-flex align-items-center">
+  <!-- Record List -->
+  <div class="card">
+    <div class="card-header">
+    <div class="d-flex justify-content-between">
+      <h5 class="card-title">Ringkasan Bahagian</h5>
 
       {{-- Select Tahun --}}
+      <form method="GET" class="w-50 d-flex justify-content-end">
       <select name="tahun" class="selectpicker w-25" data-style="btn-default" onchange="this.form.submit()">
         <option value="">Semua Tahun</option>
         @foreach(range(2024, now()->year) as $year)
@@ -38,13 +34,12 @@
       </option>
       @endforeach
       </select>
-      </div>
-    </form>
+      </form>
 
-    <hr>
-
-    <!-- Dinamic Table -->
-    <table class="table table-hover mb-4">
+    </div>
+    </div>
+    <div class="card-datatable table-responsive">
+    <table class="datatables table table-hover">
       <thead class="table-dark table-bordered">
       <tr>
         <th class="text-center align-middle" rowspan="2" style="white-space: nowrap; width: 1%;">#</th>
@@ -72,23 +67,22 @@
       <tr>
         <td class="text-center">{{ $index + 1 }}</td> {{-- Increment index for display --}}
         <td>{{ $data->bah_ketpenu }}</td>
-        <td class="text-center">{{ $matched['pengisian'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_0'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_1'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_2'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_3'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_4'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_5'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_6'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_7'] ?? 0}}</td>
-        <td class="text-center">{{ $matched['hari_8_keatas'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['pengisian'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_0'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_1'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_2'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_3'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_4'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_5'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_6'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_7'] ?? 0}}</td>
+        <td class="text-center py-5">{{ $matched['hari_8_keatas'] ?? 0}}</td>
       </tr>
     @endforeach
       </tbody>
     </table>
-
     </div>
   </div>
-  <!--/ Record List -->
+  <!--/Record List -->
 
 @endsection
